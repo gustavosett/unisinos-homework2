5411455adc90c1ccca205758a83cb82b113ce206
Gustavo Ribeiro <99373133+gustavosett@users.noreply.github.com>
2023-04-27 02:01:55 -0300

    Create Labirinto.java

---
+++ Etapa1/Labirinto.java
@@ -0,0 +1,68 @@
+package Etapa1;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Labirinto {
+    private char[][] labirinto;
+    private int linhas;
+    private int colunas;
+
+    public Labirinto() {
+    }
+
+    public void criaLabirinto(String filename) {
+        System.out.println("Caminho do projeto: " + System.getProperty("user.dir"));
+        File file = new File(System.getProperty("user.dir") + File.separator + filename);
+        System.out.println("Caminho absoluto do arquivo: " + file.getAbsolutePath());
+
+        ArrayList<String> linhasLabirinto = new ArrayList<>();
+    
+        try {
+            Path path = Paths.get(System.getProperty("user.dir"), filename);
+            linhasLabirinto = (ArrayList<String>) Files.readAllLines(path);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    
+        linhas = linhasLabirinto.size();
+        colunas = linhasLabirinto.get(0).length();
+        labirinto = new char[linhas][colunas];
+    
+        for (int i = 0; i < linhas; i++) {
+            for (int j = 0; j < colunas; j++) {
+                labirinto[i][j] = linhasLabirinto.get(i).charAt(j);
+            }
+        }
+    }
+
+    public boolean percorreLabirinto() {
+        return percorreLabirintoRecursivo(0, 0);
+    }
+    
+    private boolean percorreLabirintoRecursivo(int i, int j) {
+        if (i < 0 || i >= linhas || j < 0 || j >= colunas || labirinto[i][j] == 'X' || labirinto[i][j] == '*') {
+            return false;
+        }
+    
+        if (labirinto[i][j] == 'D') {
+            return true;
+        }
+    
+        labirinto[i][j] = '*';
+    
+        if (percorreLabirintoRecursivo(i - 1, j) ||
+            percorreLabirintoRecursivo(i + 1, j) ||
+            percorreLabirintoRecursivo(i, j - 1) ||
+            percorreLabirintoRecursivo(i, j + 1)) {
+            return true;
+        }
+    
+        labirinto[i][j] = ' ';
+        return false;
+    }    
+}
