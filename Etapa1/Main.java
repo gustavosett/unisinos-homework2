4ab6b2a1948051ffa04cb76e87c6ee8edf317ee9
Gustavo Ribeiro <99373133+gustavosett@users.noreply.github.com>
2023-04-27 02:04:33 -0300

    Update Main.java

--- Etapa1/Main.java
+++ Etapa1/Main.java
@@ -3,8 +3,9 @@ package Etapa1;
 public class Main {
     public static void main(String[] args) {
         Labirinto labirinto = new Labirinto();
-        labirinto.criaLabirinto("labirinto.txt");
+        labirinto.criaLabirinto("Etapa1/labirinto.txt");
         boolean temSaida = labirinto.percorreLabirinto();
+        labirinto.imprimirLabirinto();
         System.out.println("O labirinto " + (temSaida ? "tem saída." : "não tem saída."));
     }
 }
